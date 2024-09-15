9fd720a4470d016fab21e006227dca6983304ddf
Chrisfelix162 <chrisfelix162@gmail.com>
2024-09-15 13:09:55 -0700

    Changed param name

--- PowerShell/Export-gptPrompt.ps1
+++ PowerShell/Export-gptPrompt.ps1
@@ -1,13 +1,12 @@
 # Define your API key and the OpenAI API endpoint
 $endpoint = "https://api.openai.com/v1/chat/completions"
-$akey = 'sk-RRYtvM8sBb27cEbatqq4pv1O_qBOrvFRLE_29NNoJoT3BlbkFJ-RfwNq2mKgnwhPSejoaP3-PMifaKdWZ3GeLoBw8lkA'
 
 function prompt-OpenAI
 {
 param(
     [string]$model = "gpt-4o-mini",
     $prompt,
-    $akey,
+    $api,
     $filePath
 )
 $chatInput = @{
@@ -26,7 +25,7 @@ $chatInput = @{
 
 # Send the request to OpenAI API
 $response = Invoke-RestMethod -Uri $endpoint -Method Post -Headers @{
-    "Authorization" = "Bearer $aKey"
+    "Authorization" = "Bearer $api"
     "Content-Type"  = "application/json"
 } -Body $chatInput
 
@@ -37,4 +36,4 @@ $chatResponse | Out-File -FilePath $filePath
 }
 prompt-OpenAI -prompt "Export the your response using comma seperated values, with headers `e
 , give me a list of jobs I can apply for if I have my CCNA and A+, with the job title, and average salary for that job title. `e
-Only give the CSV output with no special characters within the prompt, remove comma from the value of the entry" -aKey $akey -filePath .\JobSalaries.CSV
+Only give the CSV output with no special characters within the prompt, remove comma from the value of the entry" -aKey $api -filePath .\JobSalaries.CSV
